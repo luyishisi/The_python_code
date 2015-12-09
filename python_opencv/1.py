9b896d812db1c0c9b2380fc2be49e01cad26092c
luyishisi <543429245@qq.com>
2015-12-09 19:25:20 +0800

    python_opencv

---
+++ python_opencv/1.py
@@ -0,0 +1,25 @@
+#coding:utf-8
+import cv2
+ 
+capture=cv2.VideoCapture(0)
+#将capture保存为motion-jpeg,cv_fourcc为保存格式
+size = (int(capture.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)),
+        int(capture.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)))
+#cv_fourcc值要设置对，不然无法写入，而且不报错，坑
+video=cv2.VideoWriter("VideoTest.avi", cv2.cv.CV_FOURCC('I','4','2','0'), 30, size)
+#isopened可以查看摄像头是否开启
+print capture.isOpened()
+num=0
+#要不断读取image需要设置一个循环
+while True:
+    ret,img=capture.read()
+    #视频中的图片一张张写入
+    video.write(img)
+    cv2.imshow('Video',img)
+    key=cv2.waitKey(1)#里面数字为delay时间，如果大于0为刷新时间，
+    #超过指定时间则返回-1，等于0没有返回值,但也可以读取键盘数值，
+    cv2.imwrite('%s.jpg'%(str(num)),img)
+    num=num+1
+    if key==ord('q'):#ord为键盘输入对应的整数,
+        break
+video.release()
---
+++ python_opencv/2.py
@@ -0,0 +1,51 @@
+#coding=utf-8
+import os
+from PIL import Image, ImageDraw
+import cv
+
+def detect_object(image):
+    '''检测图片，获取人脸在图片中的坐标'''
+    grayscale = cv.CreateImage((image.width, image.height), 8, 1)
+    cv.CvtColor(image, grayscale, cv.CV_BGR2GRAY)
+
+    cascade = cv.Load("/usr/share/opencv/haarcascades/haarcascade_frontalface_alt_tree.xml")
+    rect = cv.HaarDetectObjects(grayscale, cascade, cv.CreateMemStorage(), 1.1, 2,
+        cv.CV_HAAR_DO_CANNY_PRUNING, (20,20))
+
+    result = []
+    for r in rect:
+        result.append((r[0][0], r[0][1], r[0][0]+r[0][2], r[0][1]+r[0][3]))
+
+    return result
+
+def process(infile):
+    '''在原图上框出头像并且截取每个头像到单独文件夹'''
+    image = cv.LoadImage(infile);
+    if image:
+        faces = detect_object(image)
+
+    im = Image.open(infile)
+    path = os.path.abspath(infile)
+    save_path = os.path.splitext(path)[0]+"_face"
+    try:
+        os.mkdir(save_path)
+    except:
+        pass
+    if faces:
+        draw = ImageDraw.Draw(im)
+        count = 0
+        for f in faces:
+            count += 1
+            draw.rectangle(f, outline=(255, 0, 0))
+            a = im.crop(f)
+            file_name = os.path.join(save_path,str(count)+".jpg")
+     #       print file_name
+            a.save(file_name)
+
+        drow_save_path = os.path.join(save_path,"out.jpg")
+        im.save(drow_save_path, "JPEG", quality=80)
+    else:
+        print "Error: cannot detect faces on %s" % infile
+
+if __name__ == "__main__":
+    process("psb.jpg")
