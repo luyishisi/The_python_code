b4c49b170174030027d9f11987d05b7480d1c973
luyishisi <543429245@qq.com>
2015-12-21 21:40:21 +0800

    整理了文件结构

---
+++ python_Dragonfly/test.py
@@ -0,0 +1,12 @@
+from dragonfly.all import Grammar, CompoundRule
+
+# Voice command rule combining spoken form and recognition processing.
+class ExampleRule(CompoundRule):
+    spec = "do something computer"                  # Spoken form of command.
+    def _process_recognition(self, node, extras):   # Callback when command is spoken.
+        print "Voice command spoken."
+
+# Create a grammar which contains and loads the command rule.
+grammar = Grammar("example grammar")                # Create a grammar to contain the command rule.
+grammar.add_rule(ExampleRule())                     # Add the command rule to the grammar.
+grammar.load()                                      # Load the grammar.
