6ad469f96fc356368819b40d986b110b314a2695
luyishisi <543429245@qq.com>
2016-01-23 11:53:52 +0800

    log-16.1.23.txt

--- python_aiml_test/chatterbot/README.md
+++
@@ -1,3 +0,0 @@
-#Use https://github.com/gunthercox/ChatterBot to built my own rebot.
-###But there is a lack of train data, so rebot is not smart than aiml are.
-###Please use `cd ../start` to run aiml rebot, enjoy yourself.
--- python_aiml_test/chatterbot/test_chatterbot.py
+++
@@ -1,9 +0,0 @@
-from chatterbot import ChatBot
-chatbot = ChatBot("chcc")
-
-chatbot.train("chatterbot.corpus.english")
-chatbot.train("chatterbot.corpus.english.greetings")
-chatbot.train("chatterbot.corpus.english.conversations")
-while(True):
-    message = raw_input("you say: ")
-    chatbot.get_response(message)
